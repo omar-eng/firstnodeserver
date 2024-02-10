4c95fe7459c5bea81d9af029d3838af640feafef
omar0day <elomar10234@gmail.com>
2024-02-10 04:23:20 +0200

    newcommit

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const path = require("path");
 const userRoutes = require("./routes/userRoutes");
 const todosRoutes = require("./routes/todosRoutes");
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 const port = 3300;
 
+// console.log(process.env)
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("combined"));
-app.use(cors())
-// app.use(express.static("public"));
+app.use(cors());
 
 // Routes
 app.use("/users", userRoutes);
 app.use("/todos", todosRoutes);
 
 // Database Connection
-const dbURI = `mongodb+srv://omar010:${process.env.mongopass}@cluster0.zlqj0ym.mongodb.net/?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+const dbURI = process.env.MONGO_URL;
+mongoose.connect(dbURI)
   .then(() => console.log("DB Connected Successfully"))
   .catch((err) => console.log(`Something went wrong with the database: ${err}`));
 
